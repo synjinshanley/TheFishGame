d31ee813b04526d48bfbc59a1e827f7b5dd21da9
synjinshaley <synjinshanley@gmail.com>
2026-04-17 08:29:02 -0400

    Add title screen

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
